077d48b48d0399f231cf85d8deecc80d58b86d94
Sarunas Dovydaitis <82829974+SarunasDov@users.noreply.github.com>
2021-10-25 18:02:13 +0300

    new shop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrekeComponent } from './components/preke/preke.component';
+import { ParduotuveComponent } from './components/parduotuve/parduotuve.component';
+import { KrepselisComponent } from './components/krepselis/krepselis.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrekeComponent,
+    ParduotuveComponent,
+    KrepselisComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
